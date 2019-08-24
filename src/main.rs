cf849fb9e55de7d173931e3a6e0ab86c22698ecf
sam <samuel_brolia@msn.com>
2019-08-24 13:23:34 +0100

    Borrowing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     //_tuples();
     //_arrays();
     //_strings();
+    _ownership();
 }
 
 fn _tuples() {
@@ -39,4 +40,51 @@ fn _strings() {
     let s = h + &w; // Concatination takes reference
 
     println!("{}", s);
+}
+
+fn take(v: Vec<i32>) {
+    println!("We took v: {}", v[10] + v[100]);
+}
+
+fn cop(a: i32, b: i32) {
+    println!("{}", a + b);
+}
+
+fn _ownership() {
+    let x = 1;
+    {
+        let a = 10;
+        // x + a; // Would not error, a in scope
+    }
+    // x + a; //would error, a not in scope
+
+    let s = String::from("String");
+    
+    //let y = s;
+    //println!("{}", s); //would error - value used here after move
+    
+    let _y = &s;
+    println!("{}", s);
+
+
+
+    // Take ownership
+    let mut v = Vec::new();
+
+    for i in 1 .. 1000 {
+        v.push(i);
+    }
+
+    take(v);
+    //println!("{}", v[5]); //Would error - v was moved to the take function
+    println!("Finished!");
+
+    
+    // Copy
+    let a = 31;
+    let b = 45;
+
+    cop(a,b);
+
+    println!("we have a: {} and b: {}", a, b);
 }
